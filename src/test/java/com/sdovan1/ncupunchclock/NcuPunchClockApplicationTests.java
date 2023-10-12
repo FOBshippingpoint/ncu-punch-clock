afce9bccd78ee8a79bf25d45ae096a12168095ae
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-10-12 23:41:27 +0800

    chore: initial commit

---
+++ src/test/java/com/sdovan1/ncupunchclock/NcuPunchClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdovan1.ncupunchclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcuPunchClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
